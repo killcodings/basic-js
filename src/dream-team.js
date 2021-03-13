a198c24fa9f7de6a28dfa1e62d58420e6494a1e4
killcodings <kontaktid@ya.ru>
2021-03-13 10:17:38 +0300

    feat: added code to dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,45 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  function trim(str) {
+    try {
+      if (str && typeof str == "string") {
+        return str.replace(/^\s*|\s*$/g, "");
+      } else {
+        return "";
+      }
+    } catch (e) {
+      return str;
+    }
+  }
+
+  if (
+    typeof members !== undefined &&
+    typeof members !== null &&
+    isNaN(members) &&
+    Array.isArray(members)
+  ) {
+    console.log(typeof members);
+    if (members.length > 0) {
+      mas = [];
+      for (let i = 0; i < members.length; i++) {
+        if (
+          typeof members[i] !== null &&
+          typeof members[i] !== undefined &&
+          isNaN(members[i])
+        ) {
+          let a = trim(members[i]).toUpperCase().split("");
+          mas.push(a[0]);
+        }
+      }
+    }
+
+    if (typeof mas !== null) {
+      return mas.sort().join("");
+    } else {
+      return false;
+    }
+  } else {
+    return false;
+  }
 };
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,44 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (Array.isArray(arr)) {
+    if (arr.length > 0) {
+      let mas = arr;
+
+      for (i = 0; i < mas.length; i++) {
+        if (arr[i] == "--discard-next" && arr[i + 1] != null) {
+          mas[i + 1] = null;
+        } else if (mas[i] == "--discard-prev" && arr[i - 1] != null) {
+          mas[i - 1] = null;
+        } else if (mas[i] == "--double-next") {
+          if (i < mas.length - 2) {
+            mas[i] = mas[i + 1];
+          }
+        } else if (mas[i] == "--double-prev") {
+          if (i > 0) {
+            mas[i - 1];
+          }
+        }
+
+        mas = filtered = mas.filter(function (el) {
+          return el != "--double-next";
+        });
+
+        mas = filtered = mas.filter(function (el) {
+          return el != "--double-prev";
+        });
+        mas = filtered = mas.filter(function (el) {
+          return el != "--discard-next";
+        });
+
+        mas = filtered = mas.filter(function (el) {
+          return el != "--discard-prev";
+        });
+
+        return mas.filter(function (el) {
+          return el != null;
+        });
+      }
+    } else return [];
+  } else throw error;
 };
